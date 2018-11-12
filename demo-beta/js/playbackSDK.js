ba03808567897f45664f94f529f64320eeca50e1
ada <11102069@qq.com>
2018-11-12 15:31:51 +0800

    github

--- demo-beta/js/playbackSDK.js
+++ demo-beta/js/playbackSDK.js
@@ -343,7 +343,7 @@
 
             if (isCustomSeek) {
                 nextTime = ft;
-                if (Math.abs(nextTime - beforeTime) >= 2) {
+                if (Math.abs(nextTime - beforeTime) >= 4) {
                     if (skipOnceSeek) {
                         console.log('自定义拖动');
                         console.log('nextTime',nextTime);
