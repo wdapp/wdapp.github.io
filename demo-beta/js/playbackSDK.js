2b00439129cb58a7d858195904ee789bdd80c8e8
ada <11102069@qq.com>
2018-11-14 17:05:46 +0800

    github

--- demo-beta/js/playbackSDK.js
+++ demo-beta/js/playbackSDK.js
@@ -1117,14 +1117,12 @@
         callback.state.init(blockDuration);
 
         util.log('callback.state', callback.state);
-
-        setTimeout(function(){
-            callback.drawsInfoRequestPool.httpRequestStream(function (data) {
-                callback.draws = data;
-                callback.isHistoryReady = true;
-                callback.drawPanel.isReady = true;
-            });
-        },5000);
+        
+        callback.drawsInfoRequestPool.httpRequestStream(function (data) {
+            callback.draws = data;
+            callback.isHistoryReady = true;
+            callback.drawPanel.isReady = true;
+        });
 
         setTimeout(function () {
             initDrawPanelInfo();
