9cb7a0f74b9c9963aca750f134902ba2302faf7f
ada <11102069@qq.com>
2019-06-23 17:07:03 +0800

    Merge branch 'master' of https://github.com/wdapp/wdapp.github.io
    # Conflicts:
    #	branches/demo-beta/.svn/wc.db
    #	github/web/test.html
    #	trunk/.svn/wc.db

--- branches/demo-webpack-bate/build/webpack.dev.conf.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf.js')
-
-const devWebpackConfig = merge(baseWebpackConfig, {
-  mode: 'development',
-  devtool: 'eval-source-map'
-})
-
-module.exports = devWebpackConfig
