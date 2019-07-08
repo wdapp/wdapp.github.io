b5fd6d998094758fd37ced9ce332db36fe0b68f0
ada <11102069@qq.com>
2019-07-08 18:20:10 +0800

    Merge branch 'master' of https://github.com/wdapp/wdapp.github.io
    # Conflicts:
    #	branches/demo-beta/.svn/wc.db
    #	github/web/test.html
    #	trunk/.svn/wc.db

--- github/dp/dp/pc.js
+++ github/dp/dp/pc.js
@@ -513,7 +513,7 @@
   var mytime = 0
   // var img = new Image()
   // alert("1.0.4")
-  console.log('1.0.14')
+  console.log('1.0.15')
   PC.prototype.showJPG = function (d) {
     var t = this
     imgeLoadComplete = false
@@ -803,7 +803,7 @@
   }
 
   PC.prototype.pageChange = function (d) {
-    console.log("v1.0.14")
+    console.log("v1.0.15")
     this.current = d
     this.isLoaded = false
 
