37738efb9cc99df11aeb9afb01fbe18d730f2eb0
ada <11102069@qq.com>
2019-10-10 15:06:05 +0800

    Merge branch 'master' of https://github.com/wdapp/wdapp.github.io
    # Conflicts:
    #	branches/demo-beta/.svn/wc.db
    #	github/web/test.html
    #	trunk/.svn/wc.db

--- branches/demo-runde/runde/config/index.js
+++ branches/demo-runde/runde/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/branches/demo-runde/runde/dist/',
 
     /**
      * Source Maps
