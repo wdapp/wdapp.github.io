3220c4e7aba969e42b1b20c5f5276229d5a56b8c
ada <11102069@qq.com>
2019-11-11 11:21:49 +0800

    Merge branch 'master' of https://github.com/wdapp/wdapp.github.io
    # Conflicts:
    #	branches/demo-beta/.svn/wc.db
    #	github/web/test.html
    #	trunk/.svn/wc.db

--- branches/demo-runde/runde-mobile/src/store/state.js
+++ branches/demo-runde/runde-mobile/src/store/state.js
@@ -15,7 +15,7 @@ function lists() {
       status: false,
       name: "Replay",
       url:
-        "http://view.csslcloud.net/api/view/callback?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&liveid=F01794FFF5E23881&recordid=A13C2DEABA26FE1F&name=我的名字超级长超级长长长长长啊特别",
+        "https://view.csslcloud.net/api/view/callback?roomid=3115C441D8B66A719C33DC5901307461&userid=B27039502337407C&liveid=9FBB8D3402787184&recordid=96C0454B9E3CE464&name=123456789",
       control: "play"
     },
     {
@@ -25,7 +25,7 @@ function lists() {
       status: true,
       name: "Live",
       url:
-        "https://view.csslcloud.net/api/view/index?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&name=罗老师",
+        "https://view.csslcloud.net/api/view/index?roomid=20E2BEC88BEF3EEB9C33DC5901307461&userid=B693062ABB8020E0&name=hello wrold",
       control: "playing"
     },
     {
@@ -35,7 +35,7 @@ function lists() {
       status: false,
       name: "Replay",
       url:
-        "http://view.csslcloud.net/api/view/callback?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&liveid=F44C1692D2D3BB0A&recordid=E71FD944C7DD5187&name=科比布莱恩特",
+        "http://view.csslcloud.net/api/view/callback?roomid=8435F7E261F04EB69C33DC5901307461&userid=920022FE264A70C1&liveid=CBDA6492291E5AD0&recordid=D606FBAFE0000829&name=密码拼接到观看连接中即可",
       control: "replay"
     },
     {
@@ -45,7 +45,7 @@ function lists() {
       status: true,
       name: "Live",
       url:
-        "https://view.csslcloud.net/api/view/index?roomid=5DBA0D47CC99A99B9C33DC5901307461&userid=C783F0F7CB77E1F3&token=111&name=罗老师",
+        "https://view.csslcloud.net/api/view/index?roomid=EE66140EDA3F25CC9C33DC5901307461&userid=B693062ABB8020E0&name=第四个直播间",
       control: "replay_disabled"
     }
   ];
--- branches/demo-runde/runde-web/src/store/state.js
+++ branches/demo-runde/runde-web/src/store/state.js
@@ -25,28 +25,28 @@ function getLists () {
       tip: '已结束',
       status: false,
       name: 'Replay',
-      url: 'http://view.csslcloud.net/api/view/callback?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&liveid=F01794FFF5E23881&recordid=A13C2DEABA26FE1F&name=我的名字超级长超级长长长长长啊特别'
+      url: 'https://view.csslcloud.net/api/view/callback?roomid=3115C441D8B66A719C33DC5901307461&userid=B27039502337407C&liveid=9FBB8D3402787184&recordid=96C0454B9E3CE464&name=123456789'
     }, {
       title: '中药一闭关刷题直播（第1期）',
       subhead: '8月15日  15:00 - 16:00 王波波',
       tip: '直播中',
       status: true,
       name: 'Live',
-      url: 'https://view.csslcloud.net/api/view/index?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&name=罗老师'
+      url: 'https://view.csslcloud.net/api/view/index?roomid=20E2BEC88BEF3EEB9C33DC5901307461&userid=B693062ABB8020E0&name=hello wrold'
     }, {
       title: '获得场景视频测试回放（第2期）',
       subhead: '8月16日  15:00 - 16:00 王波波',
       tip: '已结束',
       status: false,
       name: 'Replay',
-      url: 'http://view.csslcloud.net/api/view/callback?roomid=907DE174D77ED2C39C33DC5901307461&userid=C783F0F7CB77E1F3&liveid=F44C1692D2D3BB0A&recordid=E71FD944C7DD5187&name=科比布莱恩特'
+      url: 'http://view.csslcloud.net/api/view/callback?roomid=8435F7E261F04EB69C33DC5901307461&userid=920022FE264A70C1&liveid=CBDA6492291E5AD0&recordid=D606FBAFE0000829&name=密码拼接到观看连接中即可'
     }, {
       title: '中药一闭关刷题直播（第23432423432432423423432432期）',
       subhead: '8月17日  15:00 - 16:00 王波波',
       tip: '直播中',
       status: true,
       name: 'Live',
-      url: 'https://view.csslcloud.net/api/view/index?roomid=5DBA0D47CC99A99B9C33DC5901307461&userid=C783F0F7CB77E1F3&token=111&name=罗老师'
+      url: 'https://view.csslcloud.net/api/view/index?roomid=EE66140EDA3F25CC9C33DC5901307461&userid=B693062ABB8020E0&name=第四个直播间'
     }
   ]
   return lists
