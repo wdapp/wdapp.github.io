02faac0dc454cffd8cd9767db8db093be8d131ad
ada <11102069@qq.com>
2019-11-11 10:22:26 +0800

    Merge branch 'master' of https://github.com/wdapp/wdapp.github.io
    # Conflicts:
    #	branches/demo-beta/.svn/wc.db
    #	github/web/test.html
    #	trunk/.svn/wc.db

---
+++ branches/demo-runde/runde-mobile/dist/precache-manifest.74e786e5ff44079068a7ed7462ddc94e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9bb196a1e89d6f9ad3",
+    "url": "/css/app.063317e9.css"
+  },
+  {
+    "revision": "d626a19b3bc00178df41",
+    "url": "/css/chunk-vendors.186cb7c3.css"
+  },
+  {
+    "revision": "229cdfbb57ea12c09d02c30ed4dfb5fe",
+    "url": "/img/01.229cdfbb.png"
+  },
+  {
+    "revision": "c212ea2f6a932b22a50e17d12bf99def",
+    "url": "/img/03.c212ea2f.png"
+  },
+  {
+    "revision": "10a82b66d3d065cc32be6193d58d7a76",
+    "url": "/img/04.10a82b66.png"
+  },
+  {
+    "revision": "67d1736a83477c0f6a9c69780a131909",
+    "url": "/img/big-play@3x.67d1736a.png"
+  },
+  {
+    "revision": "3e49ee7185d9ebd6e9681a9e816a0663",
+    "url": "/img/course.3e49ee71.png"
+  },
+  {
+    "revision": "6f7dc16bbefa602a5a81ba66b3aaa291",
+    "url": "/img/fail.6f7dc16b.png"
+  },
+  {
+    "revision": "6f7dc16bbefa602a5a81ba66b3aaa291",
+    "url": "/img/fail@2x.6f7dc16b.png"
+  },
+  {
+    "revision": "6f7dc16bbefa602a5a81ba66b3aaa291",
+    "url": "/img/fail@3x.6f7dc16b.png"
+  },
+  {
+    "revision": "9c2bf800acebe99b202e8bf05ba6ccff",
+    "url": "/img/header.9c2bf800.png"
+  },
+  {
+    "revision": "79f6e3e692914dfe64970eb415caddb9",
+    "url": "/img/share@3x.79f6e3e6.png"
+  },
+  {
+    "revision": "f183870e10214d9fdf6788fdc7f523ed",
+    "url": "/img/success.f183870e.png"
+  },
+  {
+    "revision": "f183870e10214d9fdf6788fdc7f523ed",
+    "url": "/img/success@2x.f183870e.png"
+  },
+  {
+    "revision": "f183870e10214d9fdf6788fdc7f523ed",
+    "url": "/img/success@3x.f183870e.png"
+  },
+  {
+    "revision": "27163cbe54b2b7accd2991b1c4f93714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd9bb196a1e89d6f9ad3",
+    "url": "/js/app.9a7e2913.js"
+  },
+  {
+    "revision": "d626a19b3bc00178df41",
+    "url": "/js/chunk-vendors.6eca892e.js"
+  },
+  {
+    "revision": "eeff1ea639d6f1b62a7b56c27ef0a5eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
